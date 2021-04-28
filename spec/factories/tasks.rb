2f4ed80196fb9d7e5ab80c61e3e8cd34dd52e2e4
DaigoMiyoshi <daigomiyoshi@DaigoMiyoshi-no-MacBook-Pro.local>
2021-04-28 12:48:36 +0900

    Add. modify task spec firstly

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -6,4 +6,16 @@ FactoryBot.define do
     to   = Date.parse("2019/12/31")
     deadline { Random.rand(from..to) }
   end
+
+  trait :task_to_be_deleted do
+    title { 'Task to be deleted' }
+  end
+
+  trait :done do
+    status { 'done' }
+  end
+
+  trait :completed_yesterday do
+    completion_date { Time.current.yesterday }
+  end
 end
